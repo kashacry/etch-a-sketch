1862ba8f4d0152251d7bafb2ca0cbe984145728e
kashacry <116587811+kashacry@users.noreply.github.com>
2024-10-20 00:27:49 +0200

    grids turn to black only when mouse down, also some fixes on function names

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ const gridwidth = getComputedStyle(document.body).getPropertyValue("--grid-width
 const accentColor = getComputedStyle(document.body).getPropertyValue("--accent-color");
 const inactiveColor = getComputedStyle(document.body).getPropertyValue("--inactive-color");
 
-
-
-const container = document.querySelector(".container");
 const sketchArea = document.querySelector("#sketch-area");
 const slider = document.querySelector("#slider");
 const sliderValue = document.querySelector("#slider-value");
@@ -14,23 +11,33 @@ const gridToggle = document.querySelector("#grid-toggle")
 let squaresPerSide = 16;
 let gridVisible = false;
 
+let isDrawing = false;
+
 function toggleGrid() {
     gridVisible = gridVisible ? false : true;
     gridToggle.style.color = gridVisible ? accentColor : inactiveColor;
 
-    removeGridCells();
-    createGridCells();
+    removeGridSquares();
+    createGridSquares();
 }
 
-function changeBackgroundColor() {
-    this.style.backgroundColor = "black";
+function changeBackgroundColor(e) {
+    if (e.type === "mousedown") {
+        isDrawing = true;
+        e.target.style.backgroundColor = "black";
+    }
+    else if (e.type === "mouseover" && isDrawing) {
+        e.target.style.backgroundColor = "black";
+    }
+    else isDrawing = false;
 }
 
-function createGridCells() {
+function createGridSquares() {
     const numOfSquares = (squaresPerSide * squaresPerSide);
 
     for (let i = 0; i < numOfSquares; i++) {
         const gridCell = document.createElement("div");
+        let widthOrHeight = 0;
 
         if (gridVisible) {
             widthOrHeight = `${(parseInt(gridwidth) / squaresPerSide) -2}px`;
@@ -42,7 +49,11 @@ function createGridCells() {
 
         gridCell.style.width = gridCell.style.height = widthOrHeight;
 
-        gridCell.addEventListener("mouseover", changeBackgroundColor);
+        gridCell.addEventListener("mousedown", (e) => changeBackgroundColor(e));
+        gridCell.addEventListener("mouseover", (e) => changeBackgroundColor(e));
+        gridCell.addEventListener("mouseup", (e) => changeBackgroundColor(e));
+
+        gridCell.addEventListener("dragstart", (e) => {e.preventDefault()});
 
         sketchArea.appendChild(gridCell);
 
@@ -50,7 +61,7 @@ function createGridCells() {
     }
 }
 
-function removeGridCells() {
+function removeGridSquares() {
     while (sketchArea.firstChild) {
         sketchArea.removeChild(sketchArea.firstChild);
     }
@@ -62,11 +73,11 @@ slider.oninput = function () {
 
     sliderValue.textContent = `${this.value} x ${this.value} (Resolution)`;
 
-    removeGridCells();
-    createGridCells(this.value);
+    removeGridSquares();
+    createGridSquares(this.value);
 }
 
 gridToggle.addEventListener("click", toggleGrid);
 
-createGridCells();
+createGridSquares();
 
